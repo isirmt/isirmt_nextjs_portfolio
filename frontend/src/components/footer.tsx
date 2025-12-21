fe298873c225be29e48bb0b89f5144eaa2b36e81
isirmt <52734487+isirmt@users.noreply.github.com>
2025-12-21 22:59:45 +0900

    fix: footer z-index

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -106,33 +106,35 @@ export default function Footer() {
   }, []);
 
   return (
-    <footer ref={footerRef} className="relative -z-1 bg-[#67c8e6]">
+    <footer ref={footerRef} className="relative bg-[#67c8e6]">
       <div
         ref={sketchContainerRef}
-        className="pointer-events-none absolute top-0 left-0 -z-1 h-full w-full"
+        className="pointer-events-none absolute top-0 left-0 z-0 h-full w-full"
       />
-      <section className="flex flex-col items-center justify-center py-12">
-        <div className={`text-4xl text-white ${delaGothicOne.className}`}>
-          情報発信中サイト
-        </div>
-        <div className="my-10 flex gap-12 text-white">
-          <InformationSite
-            siteUrl="https://itomiri.com"
-            feedUrl="https://itomiri.com/feed"
-            siteName="井筒ミリ オフィシャルサイト"
-            siteImagePath="/itomiri_com_ogp.png"
-            siteDescription="「井筒ミリ」名義での活動を告知・紹介するサイト"
-          />
-          <InformationSite
-            siteUrl="https://blog.isirmt.com"
-            feedUrl="https://blog.isirmt.com/feed"
-            siteName="isirmt ブログ"
-            siteImagePath="/blog_isirmt_com_ogp.png"
-            siteDescription="「isirmt」名義で技術ブログを運用中"
-          />
-        </div>
-      </section>
-      <div className="aspect-1235/110 bg-[url('/name_footer.svg')] bg-center bg-no-repeat" />
+      <div className="relative z-1">
+        <section className="flex flex-col items-center justify-center py-12">
+          <div className={`text-4xl text-white ${delaGothicOne.className}`}>
+            情報発信中サイト
+          </div>
+          <div className="my-10 flex gap-12 text-white">
+            <InformationSite
+              siteUrl="https://itomiri.com"
+              feedUrl="https://itomiri.com/feed"
+              siteName="井筒ミリ オフィシャルサイト"
+              siteImagePath="/itomiri_com_ogp.png"
+              siteDescription="「井筒ミリ」名義での活動を告知・紹介するサイト"
+            />
+            <InformationSite
+              siteUrl="https://blog.isirmt.com"
+              feedUrl="https://blog.isirmt.com/feed"
+              siteName="isirmt ブログ"
+              siteImagePath="/blog_isirmt_com_ogp.png"
+              siteDescription="「isirmt」名義で技術ブログを運用中"
+            />
+          </div>
+        </section>
+        <div className="aspect-1235/110 bg-[url('/name_footer.svg')] bg-center bg-no-repeat" />
+      </div>
     </footer>
   );
 }
